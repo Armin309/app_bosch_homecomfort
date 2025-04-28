e24936b8ee7f5a55e66c2bd0d3be4e7326e3a197
armin <armin.thies@rwth-aachen.de>
2025-04-28 13:29:11 +0200

    load_date update

--- src/db/business_layer/create_business_layer_views.py
+++ src/db/business_layer/create_business_layer_views.py
@@ -25,7 +25,8 @@ def create_business_layer_views():
         conn.autocommit = True
         cursor = conn.cursor()
 
-        # Define the time range (5 years back from today)
+        # Define the time range (5 years back from today)############## The part below is legacy from testing
+        # and is not used as it is not the standard business use case to filter history date for a range
         end_date = datetime.today()
         start_date = end_date - timedelta(days=5*365)
 
@@ -35,25 +36,46 @@ def create_business_layer_views():
                 CREATE OR REPLACE VIEW public.customer_DIM AS
                 SELECT h.customer_no, s.customer_name
                 FROM public.hub_customer h
-                LEFT JOIN public.sat_customer s
-                ON h.customer_no_HK = s.customer_no_HK
-                WHERE h.load_date BETWEEN %s AND %s;
+                LEFT JOIN (
+                    SELECT customer_no_HK, customer_name
+                    FROM public.sat_customer
+                    WHERE (customer_no_HK, load_date) IN (
+                        SELECT customer_no_HK, MAX(load_date)
+                        FROM public.sat_customer
+                        GROUP BY customer_no_HK
+                    )
+                ) s
+                ON h.customer_no_HK = s.customer_no_HK;
             """,
             "material_DIM": """
                 CREATE OR REPLACE VIEW public.material_DIM AS
                 SELECT h.material_id, s.material_text, s.SPRAS
                 FROM public.hub_material h
-                LEFT JOIN public.sat_material s
-                ON h.material_id_HK = s.material_id_HK
-                WHERE h.load_date BETWEEN %s AND %s;
+                LEFT JOIN (
+                    SELECT material_id_HK, material_text, SPRAS
+                    FROM public.sat_material
+                    WHERE (material_id_HK, load_date) IN (
+                        SELECT material_id_HK, MAX(load_date)
+                        FROM public.sat_material
+                        GROUP BY material_id_HK
+                    )
+                ) s
+                ON h.material_id_HK = s.material_id_HK;
             """,
             "sales_doc_DIM": """
                 CREATE OR REPLACE VIEW public.sales_doc_DIM AS
                 SELECT h.sales_document, s.distribution_channel, s.sales_organization, s.billing_doc_date, s.sales_group, s.PALEDGER
                 FROM public.hub_sales_doc h
-                LEFT JOIN public.sat_sale_doc_attributes s
-                ON h.sales_doc_HK = s.sales_doc_HK
-                WHERE h.load_date BETWEEN %s AND %s;
+                LEFT JOIN (
+                    SELECT sales_doc_HK, distribution_channel, sales_organization, billing_doc_date, sales_group, PALEDGER
+                    FROM public.sat_sale_doc_attributes
+                    WHERE (sales_doc_HK, load_date) IN (
+                        SELECT sales_doc_HK, MAX(load_date)
+                        FROM public.sat_sale_doc_attributes
+                        GROUP BY sales_doc_HK
+                    )
+                ) s
+                ON h.sales_doc_HK = s.sales_doc_HK;
             """,
             "transaction_FAC": """
                 CREATE OR REPLACE VIEW public.transaction_FAC AS
@@ -65,9 +87,16 @@ def create_business_layer_views():
                     SUM(s.VVR03 + s.VVR05 * 2) AS TGS,
                     SUM(s.VVR03 - s.VVR05 / 2) AS TNS
                 FROM public.link_transaction l
-                LEFT JOIN public.sat_transaction_VVR s
-                    ON l.transaction_id_HK = s.transaction_id_HK
-                WHERE l.load_date BETWEEN %s AND %s
+                LEFT JOIN (
+                    SELECT transaction_id_HK, VVR03, VVR05
+                    FROM public.sat_transaction_VVR
+                    WHERE (transaction_id_HK, load_date) IN (
+                        SELECT transaction_id_HK, MAX(load_date)
+                        FROM public.sat_transaction_VVR
+                        GROUP BY transaction_id_HK
+                    )
+                ) s
+                ON l.transaction_id_HK = s.transaction_id_HK
                 GROUP BY l.material_id, l.customer_no, l.sales_document;
             """
         }
